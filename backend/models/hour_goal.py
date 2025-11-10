3a678d05154d484a04506eec329ebb71d0022f60
Taylor Aydelotte <115733242+aydelottetaylor@users.noreply.github.com>
2025-11-10 11:39:51 -0700

    Revert "Feat/goals api"

--- backend/models/hour_goal.py
+++ backend/models/hour_goal.py
@@ -7,7 +7,6 @@ class HourGoal(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
     description = db.Column(db.String(255))
     num_hours = db.Column(db.Integer)
-    progress = db.Column(db.Float, default=0.0, nullable=False)
 
     user = db.relationship("User", back_populates="hour_goals")
 
